f1a3cc63cbf500c3572411f8ab1dc9bfb25bb824
awbwd <awbwd@DESKTOP-TIFECEI>
2018-12-27 15:18:55 +0900

    @configuration 클래스를 통째로 주입하기

--- chap05/src/main/java/config/ConfigPart2.java
+++ chap05/src/main/java/config/ConfigPart2.java
@@ -13,12 +13,23 @@ import spring.MemberPrinter;
 @Configuration
 public class ConfigPart2 {
 
-	@Autowired
+	/*@Autowired
 	private MemberDao memberDao;
 	//infoPrinter 메소드에 주입하기 위해 빈객체를 필드로 생성
 	//MemberDao 타입의 빈은 ConfigPart1 클래스에 정의되어 있음.
 	//따라서 ConfigPart2에서는 메서드를 호출하는 방식으로는 접근할 수 없다.
 	//주입에 필요한 객체를 위처럼 필드로 주입을 받은 뒤에 이것을 주입할 객체로 활용함.
+	*/
+	
+	
+	@Autowired
+	private ConfigPart1 configPart1;
+	
+	/*@configuration 어노테이션을 붙여서 스프링 빈객체가 되는 클래스라도, 
+	필요하다면 다른 빈 객체를 의존 주입 받을 수 있다.*/
+	
+	
+	
 	
 	
 	
@@ -30,7 +41,7 @@ public class ConfigPart2 {
 	@Bean
 	public MemberInfoPrinter infoPrinter() {
 		MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
-		infoPrinter.setMemberDao(memberDao);
+		infoPrinter.setMemberDao(configPart1.memberDao());
 		infoPrinter.setMemberPrinter(printer());
 		return infoPrinter;
 	}
